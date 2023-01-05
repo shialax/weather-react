e6220e23f4a223f2aecc3f2cc4f27c4bab4901b2
shialax <74329916+shialax@users.noreply.github.com>
2023-01-05 11:38:24 -0500

    changed color and changed hello to goodbye

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello from React
+        Goodbye from React
        <Weather city="Tokyo"/>
       </header>
     </div>
