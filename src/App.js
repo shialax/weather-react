2e3c4b7c4002ea25c194fe4e76cb5ce1946d634d
shialax <74329916+shialax@users.noreply.github.com>
2024-12-16 17:03:11 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,12 +10,7 @@ export default function App () {
       <div className="container">
 
       
-      <Weather defaultCity="Berlin"/>
-      <footer>
-  <a href="https://github.com/shialax/weather-react"
-  target="_blank" rel="noopener noreferrer">Open-source code,</a> by <a href="#">Shiala Campos</a> from <a href="https://www.shecodes.io/" target="_blank" 
-  rel="noopener noreferrer">SheCodes</a> and hosted on <a href="https://main--peppy-donut-ea7cdd.netlify.app/" target="_blank" rel="noopener noreferrer"> Netlify</a>
-  </footer>
+      <Weather defaultCity="Berlin"/> 
     </div>
 </div>
   );
